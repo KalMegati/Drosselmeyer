37c5fdc673bc71c252a3e3a44588eecc2b51277e
kalmegati <kalmegati@gmail.com>
2020-03-04 12:44:44 -0500

    routed basic CRUD functionality for Writer model

--- app/controllers/writers_controller.rb
+++ app/controllers/writers_controller.rb
@@ -1,2 +1,42 @@
 class WritersController < ApplicationController
+
+    def index
+        @writers = Writer.all
+    end
+
+    def show
+        @writer = Writer.find(params[:id])
+    end
+
+    def new
+        @writer = Writer.new
+    end
+
+    def create
+        @writer = Writer.new(writer_params)
+        @writer.save
+        redirect_to writer_path(@writer)
+    end
+
+    def edit
+        @writer = Writer.find(params[:id])
+    end
+
+    def update
+        @writer = Writer.find(params[:id])
+        @writer.attributes=(writer_params)
+        @writer.save
+        redirect_to writer_path(@writer)
+    end
+
+    def destroy
+        @writer = Writer.find(params[:id])
+    end
+
+    private
+
+    def writer_params
+        params.require('writer').permit(:handle, :password, :icon)
+    end
+
 end
---
+++ db/schema.rb
@@ -0,0 +1,78 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_03_04_151748) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "blurb"
+    t.text "story"
+    t.string "image"
+    t.integer "setting_id"
+    t.integer "writer_id"
+    t.integer "faction_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["faction_id"], name: "index_characters_on_faction_id"
+    t.index ["setting_id"], name: "index_characters_on_setting_id"
+    t.index ["writer_id"], name: "index_characters_on_writer_id"
+  end
+
+  create_table "factions", force: :cascade do |t|
+    t.string "name"
+    t.string "blurb"
+    t.text "story"
+    t.string "image"
+    t.integer "setting_id"
+    t.integer "writer_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["setting_id"], name: "index_factions_on_setting_id"
+    t.index ["writer_id"], name: "index_factions_on_writer_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "blurb"
+    t.text "story"
+    t.string "image"
+    t.integer "setting_id"
+    t.integer "writer_id"
+    t.integer "faction_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["faction_id"], name: "index_locations_on_faction_id"
+    t.index ["setting_id"], name: "index_locations_on_setting_id"
+    t.index ["writer_id"], name: "index_locations_on_writer_id"
+  end
+
+  create_table "settings", force: :cascade do |t|
+    t.string "title"
+    t.string "genre"
+    t.text "faction_list"
+    t.integer "aesthetic"
+    t.integer "writer_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["writer_id"], name: "index_settings_on_writer_id"
+  end
+
+  create_table "writers", force: :cascade do |t|
+    t.string "handle"
+    t.string "password"
+    t.string "icon"
+    t.integer "aesthetic"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end

bc91b9b413e4b30311753455b5f0930ec05c7310
kalmegati <kalmegati@gmail.com>
2020-03-08 23:50:38 -0400

    updated required attrs for Character

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,8 +2,8 @@ class Character < Entity
     # set_table_name "characters"
     self.table_name=("characters")
     belongs_to :writer
-    belongs_to :setting
-    belongs_to :faction
+    belongs_to :setting, optional: true
+    belongs_to :faction, optional: true
 
     scope :of_type, ->(archetype) { where("archetype = ?", archetype) }
 

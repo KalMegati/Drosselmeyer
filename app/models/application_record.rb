546b7ab5180ce8663bfb0b702ab0990fc9a5db2e
kalmegati <kalmegati@gmail.com>
2020-03-14 05:08:07 -0400

    established use of scope method #important to selectively display objects

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  scope :underwritten, -> { where("LENGTH(story) < 250") }
+  scope :important, -> { where("LENGTH(story) > 250") }
 
   def entities(ent_class)
     self.send(ent_class.to_s.downcase+"s")

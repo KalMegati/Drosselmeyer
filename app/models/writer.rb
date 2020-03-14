5685977b29adf9e532dfd3ec7429fb4e6d79d347
kalmegati <kalmegati@gmail.com>
2020-03-14 04:56:44 -0400

    Writer now validates format of icon

--- app/models/writer.rb
+++ app/models/writer.rb
@@ -7,6 +7,22 @@ class Writer < ApplicationRecord
     has_secure_password
 
     validates_uniqueness_of :handle, :password_digest, :icon
-    validates_presence_of :handle, :icon
+    validates_presence_of :handle
+
+    validate :icon_chars, :icon_length
+    validates_presence_of :icon
+
+
+    def icon_chars #ensures that icon contains only capital letters and numbers
+        unless icon.match(/[A-Z0-9]{3}/)
+            errors.add(:icon, "must match required formatting")
+        end
+    end
+
+    def icon_length #ensures that icon is exactly 3 characters long
+        unless icon.length == 3
+            errors.add(:icon, "must match required formatting")
+        end
+    end
 
 end
